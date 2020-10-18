1f8164b2f63d8d073ba6e85ae1ec5ad96384633d
anastasia21112 <anad221122@gmail.com>
2020-10-17 18:59:57 -0600

    fixes

--- app/src/main/java/com/example/productivitycac/UserDashboard.java
+++ app/src/main/java/com/example/productivitycac/UserDashboard.java
@@ -1,4 +1,95 @@
 package com.example.productivitycac;
 
-public class UserDashboard {
-}
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.activity.OnBackPressedCallback;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UserDashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_dashboard);
+
+
+    }
+
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent objEvent) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            onBackPressed();
+            return true;
+        }
+        return super.onKeyUp(keyCode, objEvent);
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
+    }
+
+    @Override
+    public void finish() {
+
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Are you sure you want to logout?");
+        builder.setTitle("ALERT");
+        builder.setCancelable(false);
+
+        builder
+                .setPositiveButton(
+                        "Yes",
+                        new DialogInterface
+                                .OnClickListener() {
+
+                            @Override
+                            public void onClick(DialogInterface dialog,
+                                                int which)
+                            {
+
+                                setContentView(R.layout.activity_login);
+                            }
+                        });
+
+        builder
+                .setNegativeButton(
+                        "No",
+                        new DialogInterface
+                                .OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog,
+                                                int which)
+                            {
+                                finish();
+                            }
+                        }
+                );
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+    }
+
+
