78784e5786a5b13c0edb54cdfa460e18a08d5dcb
daphnehliu <daphnehliu13@gmail.com>
2020-10-18 21:02:39 -0600

    progress

--- app/src/main/java/com/example/productivitycac/CreateNewTaskActivity.java
+++ app/src/main/java/com/example/productivitycac/CreateNewTaskActivity.java
@@ -16,47 +16,25 @@ public class CreateNewTaskActivity extends AppCompatActivity
 {
     private Button createButton,b1;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.createnewtask_activity);
 
+
         createButton = (Button)findViewById(R.id.create);
         createButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
 
-                TableRow layout = (TableRow) findViewById(R.id.tableRow10);
-                layout.setVisibility(View.INVISIBLE);
-
-                EditText taskName = findViewById(R.id.taskNameInput);
-                EditText taskTime = findViewById(R.id.taskTimeInput);
-
-                String taskNameString = taskName.getText().toString();
-                String taskTimeString = taskTime.getText().toString();
-
-                Intent nameIntent = new Intent(getApplicationContext(), ToDoList.class);
-                nameIntent.putExtra("taskName", taskNameString);
-                startActivity(nameIntent);
-
-                Intent timeIntent = new Intent(getApplicationContext(), ToDoList.class);
-                timeIntent.putExtra("taskTime", taskTimeString);
-                startActivity(timeIntent);
-
-                TextView receiverName_msg = (TextView) findViewById(R.id.task10);
-                Intent intentName = getIntent();
-                String newTaskName = intentName.getStringExtra("taskName");
-                receiverName_msg.setText(newTaskName);
-
-                TextView receiverTime_msg = (TextView) findViewById(R.id.task10Time);
-                Intent intentTime = getIntent();
-                String newTaskTime = intentTime.getStringExtra("taskTime");
-                receiverTime_msg.setText(newTaskTime);
-
-                layout.setVisibility(View.VISIBLE);
+                sendNewTask(view);
+                addNewTask(view);
 
             }
         });
+
+
 
      }
 
@@ -65,4 +43,36 @@ public class CreateNewTaskActivity extends AppCompatActivity
         startActivity(intent);
      }
 
+     public void addNewTask(View v){
+         TableRow layout = (TableRow) findViewById(R.id.tableRow10);
+         //layout.setVisibility(v.INVISIBLE);
+
+         TextView receiverName_msg = (TextView) findViewById(R.id.task10);
+         Intent intentName = getIntent();
+         String newTaskName = intentName.getStringExtra("taskName");
+         receiverName_msg.setText(newTaskName);
+
+         TextView receiverTime_msg = (TextView) findViewById(R.id.task10Time);
+         Intent intentTime = getIntent();
+         String newTaskTime = intentTime.getStringExtra("taskTime");
+         receiverTime_msg.setText(newTaskTime);
+
+         //layout.setVisibility(v.VISIBLE);
+     }
+
+     public void sendNewTask(View v){
+         EditText taskName = findViewById(R.id.taskNameInput);
+         EditText taskTime = findViewById(R.id.taskTimeInput);
+
+         String taskNameString = taskName.getText().toString();
+         String taskTimeString = taskTime.getText().toString();
+
+         Intent nameIntent = new Intent(this, ToDoList.class);
+         nameIntent.putExtra("taskName", taskNameString);
+         startActivity(nameIntent);
+
+         Intent timeIntent = new Intent(this, ToDoList.class);
+         timeIntent.putExtra("taskTime", taskTimeString);
+         startActivity(timeIntent);
+     }
 }
