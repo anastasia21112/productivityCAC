6beaba39f0b8a6edd8b2206c06c0f3908290d8db
anastasia21112 <anad221122@gmail.com>
2020-11-15 17:00:34 -0700

    test

--- app/src/main/java/com/example/productivitycac/ToDoList.java
+++ app/src/main/java/com/example/productivitycac/ToDoList.java
@@ -1,7 +1,10 @@
 package com.example.productivitycac;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.TableLayout;
@@ -22,13 +25,61 @@ public class ToDoList extends AppCompatActivity {
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.to_do_list);
-        tableLayout = (TableLayout) findViewById(R.id.table);
-        extractData();
+        //tableLayout = (TableLayout) findViewById(R.id.table);
+        //extractData();
+        init();
+    }
+
+    public void init() {
+        TableLayout stk = (TableLayout) findViewById(R.id.table);
+        TableRow tbrow0 = new TableRow(this);
+        TextView tv0 = new TextView(this);
+        tv0.setText(" Sl.No ");
+        tv0.setTextColor(Color.WHITE);
+        tbrow0.addView(tv0);
+        TextView tv1 = new TextView(this);
+        tv1.setText(" Product ");
+        tv1.setTextColor(Color.WHITE);
+        tbrow0.addView(tv1);
+        TextView tv2 = new TextView(this);
+        tv2.setText(" Unit Price ");
+        tv2.setTextColor(Color.WHITE);
+        tbrow0.addView(tv2);
+        TextView tv3 = new TextView(this);
+        tv3.setText(" Stock Remaining ");
+        tv3.setTextColor(Color.WHITE);
+        tbrow0.addView(tv3);
+        stk.addView(tbrow0);
+        for (int i = 0; i < 25; i++) {
+            TableRow tbrow = new TableRow(this);
+            TextView t1v = new TextView(this);
+            t1v.setText("" + i);
+            t1v.setTextColor(Color.WHITE);
+            t1v.setGravity(Gravity.CENTER);
+            tbrow.addView(t1v);
+            TextView t2v = new TextView(this);
+            t2v.setText("Product " + i);
+            t2v.setTextColor(Color.WHITE);
+            t2v.setGravity(Gravity.CENTER);
+            tbrow.addView(t2v);
+            TextView t3v = new TextView(this);
+            t3v.setText("Rs." + i);
+            t3v.setTextColor(Color.WHITE);
+            t3v.setGravity(Gravity.CENTER);
+            tbrow.addView(t3v);
+            TextView t4v = new TextView(this);
+            t4v.setText("" + i * 15 / 32 * 10);
+            t4v.setTextColor(Color.WHITE);
+            t4v.setGravity(Gravity.CENTER);
+            tbrow.addView(t4v);
+            stk.addView(tbrow);
+        }
+
     }
 
     public void addRow(String name, Double time, int rowNum)
     {
-        TableRow row = new TableRow(this.getApplicationContext());
+        TableRow row = new TableRow(this);
 
         CheckBox checkBox = new CheckBox(this);
         checkBox.setText(name);
@@ -82,6 +133,8 @@ public class ToDoList extends AppCompatActivity {
                 ArrayList<Object> task =  (ArrayList<Object>) listTasks.get(i);
                 String taskName = (String) task.get(0);
                 Double taskDuration = (Double) task.get(1);
+                Log.i("task name: ", taskName);
+                Log.i("task duration: ", "" + taskDuration);
                 addRow(taskName, taskDuration, i);
             }
 
