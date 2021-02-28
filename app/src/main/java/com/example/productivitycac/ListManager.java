d7b91ee439ef9f9b18f474d07e0a77823db9adcc
angelalayton <angelalayton1@gmail.com>
2021-02-28 15:08:40 -0700

    listManager read and write to file

--- app/src/main/java/com/example/productivitycac/ListManager.java
+++ app/src/main/java/com/example/productivitycac/ListManager.java
@@ -1,18 +1,15 @@
 package com.example.productivitycac;
 
 import android.content.Context;
-import android.util.Log;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
 
 public class ListManager {
 
@@ -98,6 +95,8 @@ public class ListManager {
 
 
 //******************************* Specific to reading and writing from files (Angela's part) *******************************
+    // Old methods for JSON
+    /*
     public String getJSON(Context context, String fileName)
     {
         String jsonString = "";
@@ -167,6 +166,128 @@ public class ListManager {
     public Map parseAllTasks(String jsonString)
     {
         return null;
+    }
+    */
+
+    // Write to list manager file
+    // Writes to file in this format
+    // listName1 \t taskName1 \t taskTime1 \t taskName2 \t taskTime2 \t \n
+    // listName2 \t taskName1 \t taskTime1 \t taskName2 \t taskTime2 \t \n
+    public void saveToTaskFile(String filePath) throws IOException
+    {
+        FileWriter writer = new FileWriter(filePath);
+
+        writer.write(tabSeparateListManager());
+
+        writer.close();
+    }
+
+    public String tabSeparateListManager()
+    {
+        String contents = "";
+        Set<String> listNames = this.listManager.keySet();
+        Object[] keyArray = listNames.toArray();
+        ArrayList<String> keyArrayList = new ArrayList<String>();
+        for(int i = 0; i < keyArray.length; i++)
+        {
+            keyArrayList.add((String) keyArray[i]);
+        }
+
+        for(int i = 0; i < keyArrayList.size(); i++)
+        {
+            String keyName = keyArrayList.get(i);
+
+            contents = contents + keyName + "\t";
+
+            for(int j = 0; j < listManager.get(keyName).size(); j++)
+            {
+                Set<String> taskNames = this.listManager.get(keyName).keySet();
+                Object[] taskKeyArray = taskNames.toArray();
+                ArrayList<String> taskKeyArrayList = new ArrayList<String>();
+                for(int k = 0; k < taskKeyArray.length; k++)
+                {
+                    taskKeyArrayList.add((String) keyArray[k]);
+                }
+
+                for(int m = 0; m < taskKeyArrayList.size(); m++)
+                {
+                    String taskName = taskKeyArrayList.get(m);
+
+                    contents = contents + taskName + "\t";
+                    contents = contents + listManager.get(taskName).get(m) + "\t";
+                }
+            }
+            contents = contents + "\n";
+        }
+
+        return contents;
+    }
+
+    // Reads information from the tab file
+    // Inputs information into the hashmaps to be displayed when application opens
+    public void readfromListManagerFile(String filePath) throws FileNotFoundException
+    {
+        File myFile = new File(filePath);
+        Scanner reader = new Scanner(myFile);
+
+        while(reader.hasNextLine())
+        {
+            String nextLine = reader.nextLine();
+            try
+            {
+                readListManagerLine(nextLine);
+            }
+            catch (Exception e) {}
+        }
+
+        reader.close();
+    }
+
+    // Reads tab separated information and inputs information into correct hashmap
+    public void readListManagerLine(String singleFileLine) throws IOException
+    {
+        // The first item in the list is always the name of the list
+        int firstTabIndex = singleFileLine.indexOf("\t");
+        String listName = singleFileLine.substring(0,firstTabIndex);
+        HashMap<String,Double> taskInformation = new HashMap<String,Double>();
+
+        String remainingInfo = singleFileLine.substring(firstTabIndex + 1);
+
+        //Turn string into char array to be read one at a time
+        char[] stringCharacters = new char[remainingInfo.length()];
+
+        for (int i = 0; i < remainingInfo.length(); i++) {
+            stringCharacters[i] = remainingInfo.charAt(i);
+        }
+
+        // Tab counter counts how many time a tab has been read in the file
+        // When the second tab is reached this indicates all information for a single task and can be put in the hashmap
+        int tabCounter = 0;
+        String taskString = "";
+
+        for(int i = 0; i < stringCharacters.length; i++)
+        {
+            Character character = stringCharacters[i];
+            taskString = taskString + character;
+
+            if (character.equals('\t'))
+            {
+                tabCounter++;
+                if (tabCounter % 2 == 0)
+                {
+                    String delimiter = "[\t]";
+                    String[] tokens = taskString.split(delimiter, 2);
+                    String taskName = tokens[0];
+                    Double averageTime = Double.parseDouble(tokens[1]);
+
+                    taskInformation.put(taskName, averageTime);
+                    taskString = "";
+                }
+
+            }
+        }
+
+        listManager.put(listName, taskInformation);
     }
 
 }
